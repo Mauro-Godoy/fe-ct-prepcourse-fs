accfaeb775a94536b859b813a8d27d859a1c8aa2
Mauro Godoy <maurogodoy051@gmail.com>
2024-03-23 21:48:16 -0300

    a

--- C6 Bucles/ejercicios/05.js
+++ C6 Bucles/ejercicios/05.js
@@ -2,6 +2,12 @@ function esVerdadero(valor) {
   // Si "valor" es verdadero, retorna "Soy verdadero".
   // Caso contrario, retorna "Soy falso".
   // Tu código:
+  if(valor===true){
+    return 'soy verdadero'
+  }else{
+    return 'soy falso'
+  }
+
 }
 
 module.exports = esVerdadero;
